24219f7355d70b46fbae4d712184aa54b0442cbd
git <kieronmcc@gmail.com>
2019-02-13 20:30:54 +0100

    Auth context update

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const authContext = React.createContext({authenticated: false});
+const authContext = React.createContext({
+    authenticated: false,
+    // this arrow function is declared here to 
+    // have autocompletion in IDE
+    login: () => {}
+});
 
 export default authContext;
